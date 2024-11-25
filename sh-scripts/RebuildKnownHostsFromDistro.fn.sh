5f49d81677ab233c96a7d0dc5aee77fb1edb7444
vlapan <vlapan@vlapan.com>
2024-11-25 15:00:25 +0300

    feat: + `KnownHosts`

---
+++ sh-scripts/RebuildKnownHostsFromDistro.fn.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if [ -z "$MMDAPP" ] ; then
+	set -e
+	export MMDAPP="$( cd $(dirname "$0")/../../../.. ; pwd )"
+	echo "$0: Working in: $MMDAPP"  >&2
+	[ -d "$MMDAPP/source" ] || ( echo "ERROR: expecting 'source' directory." >&2 && exit 1 )
+fi
+
+type RebuildActions >/dev/null 2>&1 || \
+	. "$MMDAPP/source/myx/myx.distro-source/sh-scripts/RebuildKnownHosts.fn.sh"
+
+
+RebuildKnownHostsFromDistro(){
+	if [ "$MDSC_INMODE" = "distro" ] ; then
+		RebuildKnownHosts "$@"
+	else
+		( \
+			. "$MMDAPP/source/myx/myx.distro-deploy/sh-lib/DistroShellContext.include" ; \
+			DistroShellContext --distro-path-auto
+			RebuildKnownHosts "$@"
+		)
+	fi
+}
+
+case "$0" in
+	*/sh-scripts/RebuildKnownHostsFromDistro.fn.sh) 
+
+		. "$( dirname $0 )/../sh-lib/DistroShellContext.include"
+		DistroShellContext --distro-path-auto
+		
+		RebuildKnownHostsFromDistro "$@"
+	;;
+esac
