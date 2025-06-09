b5f28b3c97530ea59de197fba8656334576a816a
Alex Kharichev <myx@myx.nz>
2025-06-09 14:26:41 +0300

    + Tools, utility

---
+++ sh-scripts/DistroDeployTools.fn.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+##
+## NOTE:
+## Designed to be able to run without distro context. Used to install required parts.
+##
+
+if [ -z "$MMDAPP" ] ; then
+	set -e
+	export MMDAPP="$( cd $(dirname "$0")/../../../.. ; pwd )"
+	echo "$0: Working in: $MMDAPP"  >&2
+	[ -d "$MMDAPP/.local" ] || ( echo "ERROR: expecting '.local' directory." >&2 && exit 1 )
+fi
+
+DistroDeployTools(){
+	local MDSC_CMD='DistroDeployTools'
+	[ -z "$MDSC_DETAIL" ] || echo "> $MDSC_CMD $@" >&2
+
+	set -e
+
+	case "$1" in
+		--make-*)
+			. "$MMDAPP/.local/myx/myx.distro-deploy/sh-lib/DistroDeployToolsMake.include"
+			set +e ; return 1
+		;;
+		--system-config-option|--custom-config-option)
+			. "$MMDAPP/.local/myx/myx.distro-deploy/sh-lib/DistroDeployToolsConfig.include"
+			set +e ; return 1
+		;;
+		--completion-*)
+			. "$MMDAPP/.local/myx/myx.distro-deploy/sh-lib/DistroDeployToolsCompletion.include"
+			set +e ; return 1
+		;;
+		''|--help)
+			echo "syntax: DistroDeployTools.fn.sh <option>" >&2
+			echo "syntax: DistroDeployTools.fn.sh [--help]" >&2
+			if [ "$1" = "--help" ] ; then
+				cat "$MMDAPP/.local/myx/myx.distro-deploy/sh-lib/HelpDistroDeployTools.text" >&2
+			fi
+			set +e ; return 1
+		;;
+		*)
+			echo "ERROR: $MDSC_CMD: invalid option: $1" >&2
+			set +e ; return 1
+		;;
+	esac
+}
+
+case "$0" in
+	*/myx/myx.distro-deploy/sh-scripts/DistroDeployTools.fn.sh)
+
+		if [ -z "$1" ] || [ "$1" = "--help" ] ; then
+			echo "syntax: DistroDeployTools.fn.sh --help" >&2
+		fi
+
+		set -e
+		DistroDeployTools "$@"
+	;;
+esac
