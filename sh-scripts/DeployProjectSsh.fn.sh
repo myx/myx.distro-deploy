250df34ac448ab47a10c43267f2efb726005586b
Alex Kharichev <myx@myx.nz>
2025-06-28 19:37:55 +0300

    * DeployProjectSsh - linux support improvements

--- sh-scripts/DeployProjectSsh.fn.sh
+++ sh-scripts/DeployProjectSsh.fn.sh
@@ -142,10 +142,10 @@ DeployProjectSshInternalPrintRemoteScript(){
 	# watch out: $(echo intentionally splits into several arguments!
 	# encode on sender side
 	tar jcf - -C "$cacheFolder/" $(echo "$deployType" | sed 's|full|sync exec|') | ( 
-		command -v openssl	>/dev/null 2>&1 && { \
+		command -v openssl	>/dev/null 2>&1 && (
 			[ -z "$MDSC_DETAIL" ] || echo "$MDSC_CMD: using 'openssl' to encode base64" >&2
-			openssl base64 -e -A || :
-		} || \
+			openssl base64 -e -A
+		) || \
 		command -v base64	>/dev/null 2>&1 && { \
 			[ -z "$MDSC_DETAIL" ] || echo "$MDSC_CMD: using 'base64' utility to encode" >&2
 			base64 -w0 || :
