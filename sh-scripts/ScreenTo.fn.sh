5b3c405a460c42eee330593301bcbd65adcea68d
Alex Kharichev <myx@myx.nz>
2025-07-01 01:05:46 +0300

    * ScreenTo: which -> command -v

--- sh-scripts/ScreenTo.fn.sh
+++ sh-scripts/ScreenTo.fn.sh
@@ -33,7 +33,7 @@ ScreenTo(){
 
 	local extraArguments="$( for argument in "$@" ; do printf '%q ' "$argument" ; done )"
 	local defaultCommand="-t '[ -x \"\`which screen\`\" ] && env SHELL=\"\`which bash || which sh\`\" \`which screen\` -q -O -U -D -R || \`which bash || which sh\`'"
-	local defaultCommand="-t command -v screen && env SHELL=\"\$(command -v bash || command -v sh)\" \$(command -v screen) -q -O -U -D -R || \$(command -v bash || command -v sh)"
+	local defaultCommand="-t 'command -v screen && env SHELL=\"\$(command -v bash || command -v sh)\" screen -q -O -U -D -R || \$(command -v bash || command -v sh)'"
 
 	local targets="$( ListSshTargets --select-projects "$filterProject" ${extraArguments:-$defaultCommand} | cut -d" " -f 2- )"
 
