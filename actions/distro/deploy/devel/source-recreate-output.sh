e79cef080bd0bf67cc0930c029dc5a5b5bce3475
Alex Kharichev <myx@myx.nz>
2025-10-13 22:07:03 +0300

    * build output paths

--- actions/distro/deploy/devel/source-recreate-output.sh
+++ actions/distro/deploy/devel/source-recreate-output.sh
@@ -2,12 +2,13 @@
 
 [ -d "$MMDAPP/source" ] || ( echo "⛔ ERROR: expecting 'source' directory." >&2 && exit 1 )
 
+[ -n "$MDSC_OUTPUT" ] || ( echo "⛔ ERROR: expecting MDSC_OUTPUT env set." >&2 && exit 1 )
 
 # will need to re-run after that.
 
 "$MDLT_ORIGIN/myx/myx.distro-source/sh-scripts/distro-source.sh" \
 	--debug \
-	--clean-output "$MMDAPP/output" \
+	--clean-output "$MDSC_OUTPUT" \
 	--print ""
 
 
@@ -16,8 +17,8 @@
 . "$MDLT_ORIGIN/myx/myx.distro-system/sh-scripts/DistroSourceCommand.fn.sh"
 
 DistroSourceCommand \
-	--source-root "$MMDAPP/source" \
-	--output-root "$MMDAPP/output" \
+	--source-root "$MDSC_SOURCE" \
+	--output-root "$MDSC_OUTPUT" \
 	--debug \
 	--import-from-source \
 	--build-distro-from-sources \
