e54498170c90294d37054b9e3bf9023e5d4a2736
Alex Kharichev <myx@myx.nz>
2025-10-26 14:28:11 +0200

    * AllActions, AllRepositories

--- sh-scripts/make-all-deploy-tasks.sh
+++ sh-scripts/make-all-deploy-tasks.sh
@@ -7,8 +7,8 @@ if [ -z "$MMDAPP" ] ; then
 	[ -d "$MMDAPP/source" ] || ( echo "⛔ ERROR: expecting 'source' directory." >&2 && exit 1 )
 fi
 
-type ListAllRepositories >/dev/null 2>&1 || \
-. "$MDLT_ORIGIN/myx/myx.distro-source/sh-scripts/ListAllRepositories.fn.sh"
+type AllNamespaces >/dev/null 2>&1 || \
+. "$MDLT_ORIGIN/myx/myx.distro-source/sh-scripts/AllNamespaces.fn.sh"
 
 DISTRO_PATH="$MMDAPP/distro"
 
